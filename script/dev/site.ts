312d5f730121dbf1b6480136bb99fd79f082ffd8
liuqi <70128222+l1uqi@users.noreply.github.com>
2023-06-08 20:41:22 +0800

    feat: 组件文档监听完成

--- script/dev/site.ts
+++ script/dev/site.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import { createServer } from 'vitepress';
+import { createLogger } from 'vite';
 import { componentPath, siteComponentsPath } from '../utils';
 
 type IComponent = {
@@ -9,23 +11,26 @@ type IComponent = {
 
 const components: Array<IComponent> = [];
 
-function copyFile(sourcePathList: Array<IComponent>, destinationPath: string) {
+function copyFiles(sourcePathList: Array<IComponent>, destinationPath: string) {
   if (!fs.existsSync(destinationPath)) {
     fs.mkdirSync(destinationPath);
   }
   sourcePathList.forEach((element) => {
     const destDirPath = `${destinationPath}/${element.name}`;
-    // 检查目标目录是否存在，如果不存在则创建目录
-    if (!fs.existsSync(destDirPath)) {
-      fs.mkdirSync(destDirPath);
-    }
-
-    const sourceStream = fs.createReadStream(element.path);
-    const destinationStream = fs.createWriteStream(
-      path.join(destDirPath, path.basename('index.md'))
-    );
-    sourceStream.pipe(destinationStream);
+    copyFile(element.path, destDirPath);
   });
+}
+
+function copyFile(sourcePath: string, destDirPath: string) {
+  // 检查目标目录是否存在，如果不存在则创建目录
+  if (!fs.existsSync(destDirPath)) {
+    fs.mkdirSync(destDirPath);
+  }
+  const sourceStream = fs.createReadStream(sourcePath);
+  const destinationStream = fs.createWriteStream(
+    path.join(destDirPath, path.basename('index.md'))
+  );
+  sourceStream.pipe(destinationStream);
 }
 
 function createdComponentsJson(
@@ -60,10 +65,64 @@ function traverseDirectory(dir: string) {
     }
   });
 }
-// 遍历组件md
-traverseDirectory(componentPath);
-// 复制文件
-copyFile(components, siteComponentsPath);
-// 生成components文件
-createdComponentsJson(components, siteComponentsPath);
-console.log('文档迁移成功');
+
+function createdComponentReadme() {
+  // 遍历组件md
+  traverseDirectory(componentPath);
+  // 复制文件
+  copyFiles(components, siteComponentsPath);
+  // 生成components json文件
+  createdComponentsJson(components, siteComponentsPath);
+  console.log('文档迁移成功');
+}
+
+function watchComponentReade(directoryPath: string) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        if (stats.isDirectory()) {
+          watchComponentReade(filePath);
+        } else if (path.extname(filePath) === '.md') {
+          fs.watchFile(filePath, () => {
+            const componentName = path.basename(path.parse(filePath).dir);
+            if (filePath) {
+              copyFile(filePath, siteComponentsPath + '/' + componentName);
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
+const logger = createLogger('info', {
+  prefix: 'seed'
+});
+
+async function devDocs() {
+  const server = await createServer('./site/docs', {
+    host: true,
+    port: 3080
+  });
+
+  await server.listen();
+
+  server.watcher.on('ready', () => {
+    logger.info(`\nServer is ready. Copy docs... \n`);
+  });
+}
+
+createdComponentReadme();
+watchComponentReade(componentPath);
+devDocs();
---
+++ site/index.md
@@ -0,0 +1,29 @@
+---
+layout: home
+
+title: onekui
+titleTemplate: Vue3中后台组件库
+
+hero:
+  name: onekui
+  text: Vue3中后台组件库
+  tagline: 开发中...
+  actions:
+    - theme: brand
+      text: 开始
+      link: /guild/quickstart.html
+    - theme: alt
+      text: 在 Github 上查看
+      link: https://github.com/onekjs/onek-ui
+
+features:
+  - icon: 💡
+    title: Vue3组件库
+    details: 基于vite打包和TypeScript开发
+  - icon: 📦
+    title: 仅供学习使用
+    details: 倾向于Vue3组件库的学习，请勿用于实际生产项目
+  - icon: 🛠️
+    title: 按需引入
+    details: 直接支持按需引入无需配置任何插件。
+---
