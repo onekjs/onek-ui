6919e64521411567d7ccc4be7c3148b131a733d8
liuqi <70128222+l1uqi@users.noreply.github.com>
2023-06-13 12:53:46 +0800

    chore: 添加vitest 测试按钮组件

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
@@ -40,7 +41,7 @@ export default defineConfig({
     }
   },
   test: {
-    environment: 'test'
+    environment: 'happy-dom'
   },
   plugins: [
     vue(),
