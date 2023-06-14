fae22e11f5e44c467c366737b5a6e9831393a1ec
xuxins <1368628937@qq.com>
2023-06-14 15:22:29 +0800

    feat: 图标

---
+++ packages/onekjs-icons/icons.vue
@@ -0,0 +1,30 @@
+<template>
+  <i :class="className" :style="style"></i>
+</template>
+
+<script lang='ts'>
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const classPrefix = 'onek-icon';
+    const props = defineProps({
+      size: Number,
+      type: String
+    });
+
+    const className = computed(() => {
+      return [classPrefix, props.type ? props.type : ''];
+    });
+
+    const style = computed(() => {
+      return { fontSize: props.size ? props.size + 'px' : '16px' };
+    });
+
+    return {
+      className,
+      style
+    }
+  }
+});
+</script>
