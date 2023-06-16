2b7f4358b612ed892c9312ebef87ba262cc4c184
liuqi <70128222+l1uqi@users.noreply.github.com>
2023-06-16 17:05:41 +0800

    fix: 修复demo-block打包

--- packages/onekjs-demo-block/vite.config.ts
+++ packages/onekjs-demo-block/vite.config.ts
@@ -2,17 +2,28 @@ import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
-export default (() => {
+export default () => {
   return {
-    plugins: [
-      vue(),
-      dts(),
-    ],
+    plugins: [vue(), dts()],
     build: {
-      lib: {
-        entry: resolve(__dirname, './src/index.ts'),
-        name: '@onekjs/demo-block',
+      target: 'modules',
+      //打包文件目录
+      outDir: './es',
+      cssCodeSplit: true,
+      rollupOptions: {
+        input: './src/index.ts',
+        external: ['vue', 'css'],
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
+        }
       },
-    },
-  }
-});
+      lib: {
+        entry: './src/index.ts',
+        name: 'onekjs-block',
+        fileName: 'onekjs-block'
+      }
+    }
+  };
+};
