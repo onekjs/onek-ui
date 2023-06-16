9e6d94f7295811233d4ba9291dd200066b02faee
c37\c37csq <1903498134@qq.com>
2023-06-16 16:32:15 +0800

    docs: update

--- packages/onekjs-site/docs/.vitepress/theme/index.js
+++ packages/onekjs-site/docs/.vitepress/theme/index.js
@@ -1,7 +1,9 @@
 import DefaultTheme from "vitepress/theme";
 import onekui from "@onekjs/ui";
-import DemoBlock from '@ruabick/vitepress-demo-block';
-import '@ruabick/vitepress-demo-block/dist/style.css';
+// import DemoBlock from '@ruabick/vitepress-demo-block';
+// import '@ruabick/vitepress-demo-block/dist/style.css';
+import DemoBlock from '@onekjs/demo-block';
+import '@onekjs/demo-block/dist/style.css';
 
 export default {
   ...DefaultTheme,
